c20a94a96d390868e8b2b5ea41cacac7c3517bbf
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 11:46:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-frog-dev-102227",
+  url: "https://lucky-frog-dev-102227.botics.co",
+  api: "https://lucky-frog-dev-102227.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
